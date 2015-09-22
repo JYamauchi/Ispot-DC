8bd82bac739f5b60ea1e01ce136d095dce305aaa
Jared Yamauchi <jared.yamauchi@gmail.com>
2015-09-21 23:00:01 -0400

    Add Geojson data

--- Ispot-DC/app/assets/javascripts/map.js
+++ Ispot-DC/app/assets/javascripts/map.js
@@ -2,5 +2,8 @@ $(document).ready(function) {
 	// Provide your access token
 	L.mapbox.accessToken = 'pk.eyJ1IjoiaGF3YWlpemVybyIsImEiOiJlMmRlM2YwM2VlZWExODE0MGJhNzY2OGZlYzNlMDY2NiJ9.bb6hiEKb0vpXFkNRB4y78w';
 	// Create a map in the div #map
-	var map = L.mapbox.map('map', 'hawaiizero.75dae6ca');
+	var map = L.mapbox.map('map', 'hawaiizero.ngj0npl4').setView([38.909671288923, -77.034084142948], 13);
+	var featureLayer = L.mapbox.featureLayer()
+    .loadURL('/db/Night_Club.geojson')
+    .addTo(map);
 }
